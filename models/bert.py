82e69ec03725139ef1e1c481dc6e19fa78028052
ATIpiu <1522760512@qq.com>
2021-06-04 21:52:09 +0800

    该参数

--- models/bert.py
+++ models/bert.py
@@ -21,9 +21,9 @@ class Config(object):
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)                         # 类别数
         self.num_epochs = 3                                             # epoch数
-        self.batch_size = 512                                           # mini-batch大小
-        self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
-        self.learning_rate = 5e-6                                       # 学习率
+        self.batch_size = 256                                           # mini-batch大小
+        self.pad_size = 64                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5                                       # 学习率
         self.bert_path = './bert_pretrain'
         self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
         self.hidden_size = 768
