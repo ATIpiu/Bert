0ab402f3e33b79fadd0b2dd9492296e1511ca673
ATIpiu <1522760512@qq.com>
2021-06-05 11:00:25 +0800

    该参数

--- NineNews/data/data_pre_deal.py
+++ NineNews/data/data_pre_deal.py
@@ -2,11 +2,11 @@ import csv
 
 if __name__ == '__main__':
 
-    csvFile =open('dev.csv', 'r',encoding="utf-8")
+    csvFile =open('train.csv', 'r',encoding="utf-8")
     reader = csv.reader(csvFile)
-    file=open("dev.txt", 'w+', encoding='utf-8')
+    file=open("train.txt", 'w+', encoding='utf-8')
     csvFile.readline()
     for line in reader:
         if len(line)==3:
-            file.writelines(line[2]+' \t@'+line[1]+'\n')
+            file.writelines(line[1]+' \t@'+line[2]+'\n')
         else:print(line)
