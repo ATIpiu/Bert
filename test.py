197a00fa5258bda6dcc57b3ea86d56d76755cef8
ATIpiu <1522760512@qq.com>
2021-06-14 21:30:54 +0800

    nothing

--- test.py
+++ test.py
@@ -10,12 +10,11 @@ parser = argparse.ArgumentParser(description='Chinese Text Classification')
 parser.add_argument('--model', type=str, required=True, help='choose a model: Bert, ERNIE')
 args = parser.parse_args()
 if __name__ == '__main__':
-    dataset = 'NineTitleNews'  # 数据集
+    dataset = 'content_1w_0.8'  # 数据集
 
     model_name = args.model  # bert
     x = import_module('models.' + model_name)
     config = x.Config(dataset)
-    config.test_path = dataset + '/data/test_data.txt'
     train_data, dev_data, test_data = build_dataset(config)
     test_iter = build_iterator(test_data, config)
     model = x.Model(config).to(config.device)
@@ -24,7 +23,7 @@ if __name__ == '__main__':
     start_time = time.time()
     test_acc, test_loss, test_report, test_confusion ,predict,label= evaluate(config, model, test_iter, test=True)
     file=open(dataset+"/output/OutPut.txt","w+",encoding='utf-8')
-    file1=open(dataset+"/data/test_data.txt")
+    file1=open(dataset+"/data/test.txt")
     for line,pre in zip(file1,predict) :
         file.write(line[0:-1]+','+str(pre)+'\n');
 
