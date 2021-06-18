62351573963ee23c6442764bcf9fce4af1c35ba7
ATIpiu <1522760512@qq.com>
2021-06-18 10:41:05 +0800

    nothing

--- models/xlnet.py
+++ models/xlnet.py
@@ -24,23 +24,29 @@ class Config(object):
         self.batch_size = 128  # mini-batch大小
         self.pad_size = 64  # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-5  # 学习率
-        self.bert_path = './xlnet_pretrain'
+        self.xlnet_path = './xlnet_pretrain'
         self.tokenizer = XLNetTokenizer.from_pretrained(self.xlnet_path)
         self.hidden_size = 768
 
 
-class MyXLNet(nn.Module):
+class Model(nn.Module):
     def __init__(self, config):
-        super(MyXLNet, self).__init__()
+        super(Model, self).__init__()
         self.net = XLNetModel.from_pretrained(config.xlnet_path)
-        for param in self.bert.parameters():
-            param.requires_grad = True
+        for name, param in self.net.named_parameters():
+            if 'layer.11' in name or 'layer.10' in name or 'layer.9' in name or 'layer.8' in name or 'pooler.dense' in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+        self.MLP = nn.Sequential(
+            nn.Linear(768, 10, bias=True),
+        ).to(config.device)
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
-
     def forward(self, x):
-        x = x.long()
-        x = self.net(x, output_all_encoded_layers=False).last_hidden_state
-        x = F.dropout(x, self.alpha, training=self.training)
-        x = torch.max(x, dim=1)[0]
-        x = self.MLP(x)
-        return torch.sigmoid(x)
+        context = x[0]  # 输入的句子
+        mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
+        encoder_out, text_cls = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
+        out, _ = self.lstm(encoder_out)
+        out = self.dropout(out)
+        out = self.fc_rnn(out[:, -1, :])  # 句子最后时刻的 hidden state
+        return out
